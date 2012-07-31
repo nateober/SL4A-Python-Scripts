11a37693ed79c6ddf9a96b52b1bb20f6c9b9430d
Nate Ober <nate.ober@schools.utah.gov>
2012-07-30 22:32:11 -0600

    Added resilience and redundancy. Calling from multiple sources in case one is blocked (local network policy).

--- ip.py
+++ ip.py
@@ -1,6 +1,7 @@
 import android
 import urllib
 import urllib2
+import re
 import subprocess
 
 droid = android.Android()
@@ -12,25 +13,49 @@ message = 'Please Stand By.'
 droid.dialogCreateSpinnerProgress(title, message)
 droid.dialogShow()
 
-url = 'http://automation.whatismyip.com/n09230945.asp'
-try:
-  response = urllib2.urlopen(url)
-  externalip = response.read()
-except:
-  pass
+url_increment = 0
+url_list = ['http://checkip.dyndns.org', 'http://automation.whatismyip.com/n09230945.asp']
+
+def get_external_ip(url):
+    global url_increment
+    global url_list
+    htmlstring = ''
+    ip = ''
+    try:
+        response = urllib2.urlopen(url)
+        htmlstring = response.read()
+    except:
+        pass
+    if htmlstring != '' and len(re.findall(r"\d{1,3}\.\d{0,3}\.\d{0,3}\.\d{0,3}", htmlstring)) > 0:
+        ip = re.findall(r"\d{1,3}\.\d{0,3}\.\d{0,3}\.\d{0,3}", htmlstring)[0]
+    else:
+        if url_increment < len(url_list):
+            url_increment=url_increment+1
+            ip = get_external_ip(url_list[url_increment])
+    if ip == '':
+        ip = "Not Found"
+    return ip
+
+externalip=get_external_ip(url_list[url_increment])
 
 term = subprocess.Popen('netcfg', shell=False, stdout=subprocess.PIPE).communicate()[0]
 
 for line in term.split('\n'):
-	try: 
-		if "UP" in line and not "127.0.0.1" in line and not "0.0.0.0" in line:
-			internalip = line.split()[2]
-	except:
-		pass
+    try: 
+        if "UP" in line and not "127.0.0.1" in line and not "0.0.0.0" in line:
+            internalip = line.split()[2]
+    except:
+        pass
+
 
 droid.dialogDismiss()
 title = 'IP'
-message = 'Internal IP: ' + internalip + '\nExternal IP: '+ externalip
+if internalip == externalip:
+  message = 'IP: '+ externalip
+else:
+  message = 'Internal IP: ' + internalip + '\nExternal IP: '+ externalip 
+
+print message
 droid.dialogCreateAlert(title, message)
 droid.dialogSetPositiveButtonText('Continue')
 droid.dialogShow()
